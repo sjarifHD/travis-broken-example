7b941f105897899ff722a4a6b1a09a64d8403ddd
Syarif Hidayat <rieftux@gmail.com>
2017-04-15 01:13:22 +0700

    Testing Travis CI: fixing the build again

--- Test.php
+++ Test.php
@@ -2,9 +2,8 @@
 
 class Test extends PHPUnit_Framework_TestCase
 {
-	public function testOnePlusOne() {
-		$this->assertEquals(2=1+1,1);
-  	}
+    public function testOnePlusOne()
+    {
+        $this->assertEquals(2, 1+1);
+    }
 }
-
-?>
